b725016fc7b07b7ec01aacc26a798cce9eef1092
ZakAttack <Zakt@shufersal.co.il>
2025-06-30 17:17:58 +0300

    Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    host: true,
-    https: false // Explicitly disable HTTPS
-  }
-})
+  // server: {
+  //   port: 5173,
+  //   host: true,
+  //   https: false // Explicitly disable HTTPS
+  // }
+  build: {
+    outDir: "dist",
+  },
+});
